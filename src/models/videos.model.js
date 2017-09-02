188b57749fd1593ff2c9666d3072fae13c9f5e2f
Andrea Falzetti <afalzetti@gmail.com>
2017-09-02 18:02:25 +0100

    Add attributes to videos entity

--- src/models/videos.model.js
+++ src/models/videos.model.js
@@ -7,6 +7,16 @@ module.exports = function (app) {
   const videos = new mongooseClient.Schema({
     title: { type: String, required: true },
     category: { type: String, required: true },
+    secondsDuration: {
+      type: Number,
+      min: [ 1, 'The content must be at least 1 second long' ],
+      max: [ 21600, 'The content cannot be at longer than 6 hours' ],
+    },
+    status: {
+      type: String,
+      default: 'published',
+      enum: ['published', 'unpublished']
+    },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
