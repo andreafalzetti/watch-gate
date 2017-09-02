70a5b6b44c394754fe4c9be908335783707a9c56
Andrea Falzetti <afalzetti@gmail.com>
2017-09-02 18:02:06 +0100

    Add attributes to streams entity

--- src/models/streams.model.js
+++ src/models/streams.model.js
@@ -1,11 +1,16 @@
-// streams-model.js - A mongoose model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// videos-model.js - A mongoose model
 // 
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const streams = new mongooseClient.Schema({
-    text: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+    video: { type: Schema.Types.ObjectId, ref: 'videos', required: true },
+    lastMinuteWatch: { type: Number, min: 0, max: 360 },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
