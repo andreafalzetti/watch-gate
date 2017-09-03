ba0a7c64dbee00562e6c677312692a4678532552
Andrea Falzetti <afalzetti@gmail.com>
2017-09-03 16:23:23 +0100

    Include watch-gate service

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js');
 const streams = require('./streams/streams.service.js');
 const videos = require('./videos/videos.service.js');
 const catalog = require('./catalog/catalog.service.js');
+const watchGate = require('./watch-gate/watch-gate.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
   app.configure(streams);
   app.configure(videos);
   app.configure(catalog);
+  app.configure(watchGate);
 };
