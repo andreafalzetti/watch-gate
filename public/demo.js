17eb4e9362c10d8da5d3325d1a4720c980a305fe
Andrea Falzetti <afalzetti@gmail.com>
2017-09-03 23:16:49 +0100

    Refresh user streams UI functionality

--- public/demo.js
+++ public/demo.js
@@ -1,6 +1,29 @@
 const API_URL = 'http://localhost:3030';
 let availableVideos;
 
+const refreshUser = async (user) => {
+  (async () => {
+    try {  
+      let users = await fetch(`${API_URL}/users/${user}`);
+      
+      let parsedUsers = await users.json();
+      updateUserStreams(parsedUsers)
+    } catch (error) {
+      console.log(error);
+    }
+  })();
+};
+
+const stopStream = (stream) => {
+  fetch(`${API_URL}/streams/${stream}`, {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+    .then(res => console.log(res));
+};
+
 const requestToWatch = (user) => {
   const video = $(`#videos_${user}`).val();
   // console.log('requestToWatch ==>', user, video)
@@ -30,6 +53,26 @@ const getVideosDropdown = (userId, videos) => {
   return videosDropdown;
 };
 
+const updateUserStreams = (user) => {
+  console.log('updateUser', typeof user, user)
+  $(`#${user._id} .watching`).slideUp('slow', () => {
+    let { streams = [] } = user;
+    if (typeof streams._id !== 'undefined') {
+      streams = [streams]
+    }
+    console.log('streams =>', streams)
+    let userStramsEl = '<span class="pl-4 pb-5">Nothing</span>';
+    if (streams && streams.length > 0) {
+      userStramsEl = streams.reduce((accumulator, stream) => {
+        accumulator += `<li class="list-group-item">${stream.videos.title} <button onClick="stopStream('${stream._id}')" class="btn btn-link btn-sm">Stop</button></li>`;
+        return accumulator;
+      }, '');
+      $(`#${user._id} .watching`).html(userStramsEl).slideDown(400);
+
+    }
+  });
+};
+
 const renderUsers = (users) => {
   console.log('renderUsers', typeof users, users)
   users.map((user, index) => {
@@ -42,14 +85,14 @@ const renderUsers = (users) => {
     let userStramsEl = '<span class="pl-4 pb-5">Nothing</span>';
     if (streams && streams.length > 0) {
       userStramsEl = streams.reduce((accumulator, stream) => {
-        accumulator += `<li class="list-group-item">${stream.videos.title} <button class="btn btn-secondary btn-sm">Stop</button></li>`;
+        accumulator += `<li class="list-group-item">${stream.videos.title} <button onClick="stopStream('${stream._id}')" class="btn btn-link btn-sm">Stop</button></li>`;
         return accumulator;
       }, '');
     }
     console.log(user._id, streams)
     const videoDropdown = getVideosDropdown(user._id, availableVideos)
     const userEl = `
-      <div class="col-md-4">
+      <div class="col-md-4" id="${user._id}">
       <div class="card" style="width: 20rem;">
         <img class="card-img-top" src="http://via.placeholder.com/350x150&text=${user.firstName}" alt="Card image cap">
         <div class="card-block">
@@ -61,9 +104,9 @@ const renderUsers = (users) => {
           </div>
         </div>
         <div class="card-block">
-          <h4 class="card-title pl-4 pt-4">Watching</h4>
+          <h4 class="card-title pl-4 pt-4">Watching <button class="btn btn-link btn-sm" onClick="refreshUser('${user._id}')"><i class="fa fa-refresh" aria-hidden="true"></i></button></h4>                    
         </div>
-        <ul class="list-group list-group-flush">${userStramsEl}</ul>
+        <ul class="list-group list-group-flush watching">${userStramsEl}</ul>
         <div class="card-block p-4">
           <h5>Watch new video</h5>
           ${videoDropdown}
