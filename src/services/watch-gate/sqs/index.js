a15b12fca5d3ba2b59fd92b151673f7c3e1fd245
Andrea Falzetti <afalzetti@gmail.com>
2017-09-03 22:28:21 +0100

    Code clean-up

--- src/services/watch-gate/sqs/index.js
+++ src/services/watch-gate/sqs/index.js
@@ -1,12 +1,6 @@
 const SQSConsumer = require('./SQSConsumer');
 
-/**
- * Pre-loads all handlers
- */
-// const SQSHandlers = require('require-all')(__dirname + '/handlers');
-
 const createSQSConsumer = (options) => {
-  // options.handlers = SQSHandlers;
   const consumer = new SQSConsumer(options);
   return consumer;
 };
